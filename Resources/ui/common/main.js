742bdba2e9383894b4c4c24e98571ae54d47bb64
Addiel <addieljuarez@gmail.com>
2013-08-31 13:03:42 -0500

    dia 1

--- Resources/ui/common/main.js
+++ Resources/ui/common/main.js
@@ -3,11 +3,24 @@
  */
 function MainWindow(){
 	var self = Titanium.UI.createWindow({
-		backgroundColor:'#000',
+		backgroundColor:'#fff',
+		// layout:'vertical',
+		layout:'horizontal',
 	});
 	
+	var ejemplo = Titanium.UI.createLabel({
+		text:'ejemplo',
+		color:'#000',
+	});
 	
+	var boton = Titanium.UI.createButton({
+		title:'test',
+		height:40,
+		width:100,
+	});
 	
+	self.add(boton);
+	self.add(ejemplo);
 	
 	return self;
 }
