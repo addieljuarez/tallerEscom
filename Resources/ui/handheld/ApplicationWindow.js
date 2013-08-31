b4f8276b2b597be94dc89600a649afa13cbd4ad2
Addiel <addieljuarez@gmail.com>
2013-08-31 12:26:00 -0500

    animation

--- Resources/ui/handheld/ApplicationWindow.js
+++ Resources/ui/handheld/ApplicationWindow.js
@@ -28,20 +28,20 @@ function ApplicationWindow() {
 	});
 	
 	var password = Titanium.UI.createTextField({
-		top:200,
+		top:250,
 		height:30,
 		width:200,
 		hintText:'aqui pon tu pass',
 		borderColor:'#000',
 		passwordMask:true,
+		returnKeyType:Titanium.UI.RETURNKEY_GO,
 	});
 	
 	var enviar = Titanium.UI.createButton({
-		top:300,
+		top:350,
 		height:40,
 		width:150,
 		title:'envia',
-		// backgroundImage:'none',
 	});
 	
 	
@@ -49,14 +49,43 @@ function ApplicationWindow() {
 		// alert('click en la ventana');
 		nombre.blur();
 		password.blur();
+		var bajar = Ti.UI.createAnimation({duration:500, top:0});
+		self.animate(bajar);
 	});
+	
+	
+	nombre.addEventListener('return', function(e){
+		password.focus();
+		
+		var subir = Ti.UI.createAnimation({duration:500, top:-70});
+		self.animate(subir);
+	});
+	
+	password.addEventListener('return', function(e){
+		var bajar = Ti.UI.createAnimation({duration:500, top:0});
+		self.animate(bajar);
+	});
+	
+	
+	password.addEventListener('focus', function(e){
+		var subir = Ti.UI.createAnimation({duration:300, top:-70});
+		self.animate(subir);
+	});
+	
+	
+	
+	enviar.addEventListener('click', function(e){
+		
+	});
+	
+	
+	
 	
 	self.add(saludo);
 	self.add(nombre);
 	self.add(password);
 	self.add(enviar);
 	return self;
-
 }
 
 //make constructor function the public component interface
